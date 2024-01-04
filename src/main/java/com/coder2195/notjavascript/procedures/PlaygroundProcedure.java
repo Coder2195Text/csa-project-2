e188f1a94a607995df2b77c6a2ddc8b18a04de37
Coder2195 <coder2195mail@gmail.com>
2024-01-03 21:09:16 -0500

    69420 rotten flesh

--- src/main/java/com/coder2195/notjavascript/procedures/PlaygroundProcedure.java
+++ src/main/java/com/coder2195/notjavascript/procedures/PlaygroundProcedure.java
@@ -1,17 +1,17 @@
 package com.coder2195.notjavascript.procedures;
 
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.network.chat.Component;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.CommandSource;
 
 public class PlaygroundProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		if (world instanceof ServerLevel _level)
-			_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
-					"summon not_javascript:nuclear_bomb_entity");
+		if (world instanceof ServerLevel _level) {
+			ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(Items.ROTTEN_FLESH, 69420));
+			entityToSpawn.setPickUpDelay(10);
+			_level.addFreshEntity(entityToSpawn);
+		}
 	}
 }
--- src/main/java/com/coder2195/notjavascript/procedures/UnluckyBlockOpenProcedure.java
+++ src/main/java/com/coder2195/notjavascript/procedures/UnluckyBlockOpenProcedure.java
@@ -14,8 +14,11 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.item.PrimedTnt;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.phys.Vec3;
@@ -56,7 +59,6 @@ public class UnluckyBlockOpenProcedure {
 
 	public static void carpetBombing(LevelAccessor world, double x, double y, double z, Entity entity) {
 		MinecraftServer server = world.getServer();
-		
 
 		if (server != null)
 			server.getPlayerList()
@@ -64,31 +66,38 @@ public class UnluckyBlockOpenProcedure {
 							Component.literal("SLEEPY JOE HAS AUTHORIZED A CARPET BOMBING OF THIS AREA!!!"), false);
 
 		for (int bx = 0; bx < 21; bx++) {
-			final double bxx = x + bx - 9.5 ;
+			final double bxx = x + bx - 9.5;
 			NotJavascriptMod.queueServerWork(bx * 5, () -> {
 				if (world instanceof ServerLevel level) {
 					for (double bz = z - 9.5; bz < z + 10.5; bz++) {
-						
-			level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(bxx, y + 20, bz), Vec2.ZERO, level, 4, "", Component.literal(""), level.getServer(), null).withSuppressedOutput(),
-					"summon tnt ~ ~ ~ {Fuse:40}");
 
+						level.getServer().getCommands().performPrefixedCommand(
+								new CommandSourceStack(CommandSource.NULL, new Vec3(bxx, y + 20, bz), Vec2.ZERO, level, 4, "",
+										Component.literal(""), level.getServer(), null).withSuppressedOutput(),
+								"summon tnt ~ ~ ~ {Fuse:40}");
 					}
 				}
-
-				
 			});
+		}
+	}
 
+	public static void fleshy(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (world instanceof ServerLevel level) {
+			ItemEntity e = new ItemEntity(level, x, y, z, new ItemStack(Items.ROTTEN_FLESH, 69420));
+			e.setPickUpDelay(10);
+			level.addFreshEntity(e);
 		}
 	}
 
 	public static Disaster[] disasters = {
 			UnluckyBlockOpenProcedure::hi,
 			UnluckyBlockOpenProcedure::famine,
-			UnluckyBlockOpenProcedure::carpetBombing
+			UnluckyBlockOpenProcedure::carpetBombing,
+			UnluckyBlockOpenProcedure::fleshy
 	};
 
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
-		carpetBombing(world, x, y, z, entity);
+		fleshy(world, x, y, z, entity);
 		// RandomSource rand = world.getRandom();
 		// int i = Math.abs(rand.nextInt() % disasters.length);
 		// disasters[i].execute(world, x, y, z, entity);
